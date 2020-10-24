61aecad484397a9067eefaf53bdcd592a4b1d417
Rudy <rh16216@my.bristol.ac.uk>
2020-10-24 15:54:23 +0100

    Fixed memory size issues when reading in texture file.

--- Cube/Renderer.cpp
+++ Cube/Renderer.cpp
@@ -284,31 +284,31 @@ HRESULT Renderer::LoadTexture()
 	FILE* textureFile;
 	BYTE* bytes;
 
-	size_t destSize = 4096;
+	size_t destSize = 200000;
 	size_t bytesRead = 0;
 	bytes = new BYTE[destSize];
 
 	fopen_s(&textureFile, "C:/Users/Rudy/source/repos/Cube/Cube/texture.ppm", "rb");
-	bytesRead = fread_s(bytes, destSize, 1, 4096, textureFile);
+	bytesRead = fread_s(bytes, destSize, 1, 480*395, textureFile);
 	
 	D3D11_SUBRESOURCE_DATA tData;
 	ZeroMemory(&tData, sizeof(D3D11_SUBRESOURCE_DATA));
 	tData.pSysMem = bytes;
-	tData.SysMemPitch = 0;
+	tData.SysMemPitch = 480;
 	tData.SysMemSlicePitch = 0;
 
 	D3D11_TEXTURE2D_DESC desc;
-	desc.Width = 256;
-	desc.Height = 256;
+	desc.Width = 480;
+	desc.Height = 395;
 	desc.MipLevels = desc.ArraySize = 1;
 	desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 	desc.SampleDesc.Count = 1;
+	desc.SampleDesc.Quality = 0;
 	desc.Usage = D3D11_USAGE_DYNAMIC;
 	desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
 	desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
 	desc.MiscFlags = 0;
 
-	ID3D11Texture2D *pTexture = NULL;
 	hr = device->CreateTexture2D(&desc, &tData, &m_pTexture);
 
 	CD3D11_SHADER_RESOURCE_VIEW_DESC textureViewDesc(D3D11_SRV_DIMENSION_TEXTURE2D);
